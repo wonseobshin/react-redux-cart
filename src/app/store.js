f1975233492081935bdbd1dd8cba07fd9d9aceac
Won Seob Shin <wonseob07@gmail.com>
2020-11-23 01:44:11 +0000

    now have store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 // src/app/store.js
 
 import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from '../features/rootReducer'
+//import rootReducer from '../features/rootReducer'
+//instead we can just use cartReducer from feature/cart and not have a separate file.
+import cartReducer from '../features/cart/cartSlice'
 
 const store = configureStore({
-    reducer: rootReducer
+    reducer: {
+        cart: cartReducer
+    }
 })
 
 export default store
